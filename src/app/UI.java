9b249f3dea827951ceb6131ca2266b90d66cbef0
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-31 21:28:11 -0300

    :lipstick: feat: Small change to the board print

--- src/app/UI.java
+++ src/app/UI.java
@@ -51,13 +51,13 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + "  ");
+            System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
-        System.out.println("   a  b  c  d  e  f  g  h");
+        System.out.println("  a b c d e f g h");
     }
 
     public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
@@ -71,13 +71,13 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + "  ");
+            System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], possibleMoves[i][j]);
             }
             System.out.println();
         }
-        System.out.println("   a  b  c  d  e  f  g  h");
+        System.out.println("  a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece, boolean background) {
@@ -98,7 +98,7 @@ public class UI {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
-        System.out.print("  ");
+        System.out.print(" ");
     }
 
     private static void printCapturedPieces(List<ChessPiece> captured) {
